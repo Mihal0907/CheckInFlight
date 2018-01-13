bbfb063004fa4bf56d373d2cc80f87263c571f4d
Mihal <miandmih@gmail.com>
2018-01-13 19:55:01 +0200

    Adding a first UML diagram of model package. Adding ServiceUtils, and tasks for service.

---
+++ src/ua/artcode/utils/ServiceUtils.java
@@ -0,0 +1,12 @@
+package ua.artcode.utils;
+
+public class ServiceUtils {
+
+     /**
+     * 1.show client flights
+     * 2.show all flights (one day / in range of days)
+     * 3.add new flight
+     * 4.remove flight
+     * 5.book flight
+     */
+}

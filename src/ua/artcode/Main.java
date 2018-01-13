d97b254416635083f26b1319807162faba437702
Mihal <miandmih@gmail.com>
2018-01-13 19:45:54 +0200

    Adding a first UML diagram of model package.

--- src/ua/artcode/Main.java
+++
@@ -1,8 +0,0 @@
-package ua.artcode;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/ua/artcode/tests/Service.java
@@ -0,0 +1,8 @@
+package ua.artcode.tests;
+
+public class Service {
+
+    public static void main(String[] args) {
+
+    }
+}
